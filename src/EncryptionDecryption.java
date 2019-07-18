6a5ff5d75129799227820a390da4b54763510e65
Pleshka <pleshka321@gmail.com>
2019-07-18 15:16:06 +0200

    added -in and -out command for reading and writing from a file

--- src/EncryptionDecryption.java
+++ src/EncryptionDecryption.java
@@ -1,14 +1,20 @@
 import java.util.*;
+import java.io.*;
+import java.nio.file.*;
 
 public class EncryptionDecryption {
     // A - Z = 65 - 90, a - z = 97 - 122
     public static Scanner scanner = new Scanner(System.in);
 
-
     public static void main(String[] args) {
         String EncryptOrDecrypt = "enc";
         String strOriginalText = null;
         String strKey = null;
+        String inputFilePath = null;
+        String outputFilePath = null;
+        char [] arrToEncrypt = null;
+        String inputFileString;
+        boolean data = false;
 
         for (int i = 0; i < args.length; i++)
         {
@@ -19,29 +25,61 @@ public class EncryptionDecryption {
                 strKey = args[i + 1];
 
             else if (args[i].equals("-data"))
+            {
                 strOriginalText = args[i + 1];
+                data = true;
+            }
+
+            else if (args[i].equals("-in"))
+                inputFilePath = args[i + 1];
+
+            else if (args[i].equals("-out"))
+                outputFilePath = args[i + 1];
         }
 
-        int key = Integer.parseInt(strKey);
-        char [] arrToEncrypt = strOriginalText.toCharArray();
 
+        int key = Integer.parseInt(strKey);
+
+        if (data == true)
+        arrToEncrypt = strOriginalText.toCharArray();
+
+        else
+        {
+            try
+            {
+            inputFileString = readFileAsString(inputFilePath);
+            arrToEncrypt = inputFileString.toCharArray();
+            }
+            catch (IOException e)
+            {
+                System.out.println("Cannot read file: " + e);
+            }
+        }
 
         switch (EncryptOrDecrypt)
         {
         case "enc":
-            Encrypt(arrToEncrypt, key);
+            Encrypt(arrToEncrypt, key, data, outputFilePath);
             break;
 
         case "dec":
-            Decrypt(arrToEncrypt, key);
+            Decrypt(arrToEncrypt, key, data, outputFilePath);
             break;
-
         }
 
+
     }
-    static void Encrypt(char [] arrToEncrypt, int key)
+
+
+
+    public static String readFileAsString(String fileName) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(fileName)));
+    }
+
+    static void Encrypt(char [] arrToEncrypt, int key, boolean data, String outputFilePath)
     {
         int res = 0;
+        String strCypherText = "";
         for (int i = 0; i < arrToEncrypt.length; i++)
         {
                 if (arrToEncrypt[i] + key > 127)
@@ -56,13 +94,30 @@ public class EncryptionDecryption {
                 }
         }
         for (char cypherText : arrToEncrypt)
-            System.out.print(cypherText);
+        {
+            strCypherText = strCypherText + cypherText;
+        }
+
+            if (data == true)
+            System.out.print(strCypherText);
+
+            else
+            {
+
+                File file = new File(outputFilePath);
+                try (PrintWriter printWriter = new PrintWriter(file)) {
+                    printWriter.print(strCypherText);
+                } catch (IOException e) {
+                    System.out.printf("An exception occurs %s", e.getMessage());
+                }
+            }
 
     }
 
-    static void Decrypt(char [] arrToDecrypt, int key)
+    static void Decrypt(char [] arrToDecrypt, int key, boolean data, String outputFilePath)
     {
         int res = 0;
+        String strDecypherText = "";
         for (int i = 0; i < arrToDecrypt.length; i++)
         {
                 if (arrToDecrypt[i] - key < 0)
@@ -76,8 +131,24 @@ public class EncryptionDecryption {
                     arrToDecrypt[i] = (char)res;
                 }
         }
-        for (char decypherText : arrToDecrypt)
-            System.out.print(decypherText);
+        for (char cypherText : arrToDecrypt)
+        {
+            strDecypherText = strDecypherText + cypherText;
+        }
+
+        if (data == true)
+            System.out.print(strDecypherText);
+
+        else
+        {
+
+            File file = new File(outputFilePath);
+            try (PrintWriter printWriter = new PrintWriter(file)) {
+                printWriter.print(strDecypherText);
+            } catch (IOException e) {
+                System.out.printf("An exception occurs %s", e.getMessage());
+            }
+        }
     }
 
 }
